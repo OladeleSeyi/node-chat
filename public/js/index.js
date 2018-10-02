dcd758af4afe665e360769c646f5c1a55e897116
OladeleSeyi <toothy61@gmail.com>
2018-10-02 12:23:28 +0100

    Emit newMessage and CreateMeassage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'John@johnson.som',
-    text: 'Got milk?'
-  })
 });
 
 
--- server/server.js
+++ server/server.js
@@ -19,14 +19,13 @@ io.on('connection', (socket) => {
   console.log('New User connection');
 
 
-  socket.emit('newMessage', {
-    from: 'Seyi@messages.com',
-    text: 'Where y\'all at',
-    createdAt: new Date().toString()
-  });
-
   socket.on('createMessage', (message) => {
     console.log('New message', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
 
   socket.on('disconnect', () => {
