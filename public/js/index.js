8eae3df7200c2ac9754afe6db5f2fdd495309a16
OladeleSeyi <toothy61@gmail.com>
2018-10-03 08:11:48 +0100

    time stamps with messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,8 +9,10 @@ socket.on('connect', function () {
 socket.on('newMessage', function (message) {
   console.log('got new message', message);
 
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from}: ${formattedTime} ${message.text}`);
   jQuery('#messages').append(li);
 
 })
@@ -18,8 +20,9 @@ socket.on('newMessage', function (message) {
 socket.on('newLocationMessage', function (message) {
   var li = jQuery('<li></li>');
   var a = jQuery('<a href="" target="_blank">My current location</a>');
+  var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from}:  at ${formattedTime}`);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
